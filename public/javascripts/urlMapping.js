9993c2e3f095d5cbcd2d821e00ab1c931b4ef2fb
MinionCZ <stengljirka@gmail.com>
2020-12-07 20:25:59 +0100

    style for dashboard and new gallery done

--- public/javascripts/urlMapping.js
+++ public/javascripts/urlMapping.js
@@ -1,4 +1,6 @@
+document.cookie = "mapping=dashboard"
 function changeMapping(mapping) {
+    document.cookie = "mapping=" + mapping
     switch (mapping) {
         case "dashboard":
             window.location = "/dashboard"
@@ -17,3 +19,51 @@ function changeMapping(mapping) {
             break
     }
 }
+window.addEventListener("load",setClickedButton)
+
+
+
+
+function setClickedButton(){
+    let value = parseCookie("mapping")
+    let div = document.getElementById("buttonDiv").children
+    for(var button of div){
+        if(button.value === value){
+            button.setAttribute("class", "navButton-selected")
+        }
+    }
+
+}
+/*
+parses cookies from browser by name
+*/
+function parseCookie(name){
+    let cookies = document.cookie.split(";")
+    const map = new Map()
+    for(const cookie of cookies){
+        let oreo = cookie.split("=")
+        map.set(parseName(oreo[0]), oreo[1])
+    }
+    let returnValue = null
+    if(map.has(name)){
+        returnValue = map.get(name)
+    }
+    return returnValue
+}
+
+/*
+some cookie names has space before name, this function crop the space, or return name if it is without space
+*/
+
+function parseName(name){
+    let nameToReturn = ""
+    if (name[0] === " "){
+        nameToReturn = name.substring(1)
+    }else{
+        nameToReturn = name
+    }
+    return nameToReturn
+
+}
+
+
